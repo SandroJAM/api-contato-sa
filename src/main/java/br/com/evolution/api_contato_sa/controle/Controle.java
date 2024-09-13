daac9aac4e06cb89b90cd1a28efb76485d4791e2
Sandro Muniz <sandrojam@hotmail.com>
2024-09-12 22:42:54 -0300

    Configurando Controle
    Implementando e testando nosso controlador.

--- src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
+++ src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
@@ -1,5 +1,14 @@
 package br.com.evolution.api_contato_sa.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controle {
+
+    @GetMapping("/")
+    public String teste(){
+        return "Hello World!";
+    }
     
 }
