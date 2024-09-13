e9e9b95fbd8c8d1f2f53f15949730b6a85b726b1
Sandro Muniz <sandrojam@hotmail.com>
2024-09-12 23:19:22 -0300

    Annotation @Autowired
    Indica ao Spring que uma classe precisa de uma determinada dependência. O Spring, então, cria e injeta automaticamente a instância correta da dependência no momento adequado

--- src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
+++ src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
@@ -1,10 +1,16 @@
 package br.com.evolution.api_contato_sa.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.evolution.api_contato_sa.repositorio.Repositorio;
+
 @RestController
 public class Controle {
+ 
+    @Autowired // Para não objeto instanciar esse objeto em cada rota
+    private Repositorio acao; // Objeto para cadastrar, selecionar, alterar e remover contato
 
     @GetMapping("/")
     public String teste(){
