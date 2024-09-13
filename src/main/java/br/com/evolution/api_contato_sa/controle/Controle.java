fc8526e69e771107fbc9ed757f5af3983effb99a
Sandro Muniz <sandrojam@hotmail.com>
2024-09-13 12:49:40 -0300

    Rota Editar
    Responsável pela edição dos dados dos contatos no banco de dados.

--- src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
+++ src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
@@ -3,6 +3,7 @@ package br.com.evolution.api_contato_sa.controle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,10 @@ public class Controle {
     //public String teste(){
     //    return "Hello World!";
     //}
+
+    @PutMapping("/")
+    public Contato editar(@RequestBody Contato objc){
+        return acao.save(objc);
+    }
     
 }
