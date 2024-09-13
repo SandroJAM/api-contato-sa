4ab612293b742eb4d690c582cd0fc3f0d41c83a6
Sandro Muniz <sandrojam@hotmail.com>
2024-09-12 22:57:49 -0300

    Configurando Entidade
    Implmentando nosso modelo Contato no banco de dados.

--- src/main/java/br/com/evolution/api_contato_sa/modelo/Contato.java
+++ src/main/java/br/com/evolution/api_contato_sa/modelo/Contato.java
@@ -1,5 +1,25 @@
 package br.com.evolution.api_contato_sa.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "contatos")
+@Getter
+@Setter
 public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    private String nome;
+    private String email;
+    private String nrtelefone;
     
 }
