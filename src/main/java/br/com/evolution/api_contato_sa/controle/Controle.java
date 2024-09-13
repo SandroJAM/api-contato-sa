1440b2036249dcddadfa53ec7afc7792ead1bd33
Sandro Muniz <sandrojam@hotmail.com>
2024-09-13 00:07:40 -0300

    Rota Cadastro
    Responsavél pelo cadastro de contatos no banco de dados.

--- src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
+++ src/main/java/br/com/evolution/api_contato_sa/controle/Controle.java
@@ -2,8 +2,11 @@ package br.com.evolution.api_contato_sa.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.evolution.api_contato_sa.modelo.Contato;
 import br.com.evolution.api_contato_sa.repositorio.Repositorio;
 
 @RestController
@@ -12,6 +15,12 @@ public class Controle {
     @Autowired // Para não objeto instanciar esse objeto em cada rota
     private Repositorio acao; // Objeto para cadastrar, selecionar, alterar e remover contato
 
+    @PostMapping("/")
+    public Contato cadastrar(@RequestBody Contato objc){
+
+        return acao.save(objc);
+        
+    }
     @GetMapping("/")
     public String teste(){
         return "Hello World!";
